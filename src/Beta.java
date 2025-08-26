492d2fcedde05b6d3aa813b7a670b53977c78e76
iRahmanG <rahman039@gmail.com>
2025-08-26 11:26:54 +0530

    26 Aug 2025 - Method Overriding

---
+++ src/Beta.java
@@ -0,0 +1,5 @@
+public class Beta extends Papa {
+    public void watchVideo(){
+        System.out.println("Watch video lecture of Java");
+    }
+}
---
+++ src/Papa.java
@@ -0,0 +1,6 @@
+public class Papa {
+    public void watchVideo(){
+        System.out.println("Watch video lecture of Java");
+    }
+
+}
