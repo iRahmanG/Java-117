188e1c0654e0d5013b270148ee6275940cb1c9ff
iRahmanG <rahman039@gmail.com>
2025-08-27 14:03:00 +0530

    27 Aug 2025 - Exception Handling

---
+++ src/MyException.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class MyException {
+    public static void main(String [] args){
+        int x,y,z=0;
+        Scanner sc=new Scanner(System.in);
+        try{
+            x=sc.nextInt();
+            y=sc.nextInt();
+            z=x/y;
+            System.out.println("Exception not occur");
+        } catch(ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println("Result is : "+z);
+        }
+    }
+}
